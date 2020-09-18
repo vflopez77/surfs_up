a6ded475d22baaffb653c6d18da43df951a15014
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-09-17 21:31:15 -0400

    Continuing Climate Analysis

--- app.py
+++ app.py
@@ -34,11 +34,11 @@ app = Flask(__name__)
 def welcome():
     return(
     '''
-    Welcome to the Climate Analysis API!\n
-    Available Routes:\n
-    /api/v1.0/precipitation\n
-    /api/v1.0/stations\n
-    /api/v1.0/tobs\n
+    Welcome to the Climate Analysis API!<br/>
+    Available Routes:<br/>
+    /api/v1.0/precipitation<br/>
+    /api/v1.0/stations<br/>
+    /api/v1.0/tobs<br/>
     /api/v1.0/temp/start/end
     ''')
 
